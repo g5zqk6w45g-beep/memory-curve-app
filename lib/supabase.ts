dc5550f44f9321ff52ba756c30b5c62c480c15d5
Alexmrt4 <Alexandreel2007@gmail.com>
2026-01-07 21:39:20 +0100

    Fix supabase crash build

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// Si les clés ne sont pas encore chargées (pendant le build), on met des fausses valeurs pour éviter le crash.
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://placeholder.supabase.co";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "placeholder-key";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
